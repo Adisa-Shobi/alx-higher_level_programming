910453ada0d18c27cbdc9b1ec57d047435ce6c5c
Adisa-Shobi <s.oadisa.dev@gmail.com>
2022-06-27 19:08:14 +0000

    Advanced tasks completed

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ *check_cycle - Checks if a linked list contains a cycle
+ *
+ *@list: The head of the list being queried
+ *Return: 0 if no cycle, and 1 if cycle is found
+ */
+int check_cycle(listint_t *list)
+{
+	while()
+}
