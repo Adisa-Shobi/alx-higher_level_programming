d8062ffb2d67f22015ac5df88f3a2cd1d4a660c2
Adisa-Shobi <s.oadisa.dev@gmail.com>
2022-06-28 12:21:37 +0000

    Task 1 Retry

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,7 +6,7 @@ last_digit = unsigned_num % 10
 if(number < 0 and last_digit != 0):
     last_digit *= -1
 if (last_digit == 0):
-    suffix = "and is zero"
+    suffix = "and is 0"
 elif (last_digit > 5):
     suffix = "and is greater than 5"
 else:
