5509ce2e0de4c50f216b0328ac73587912fb6f87
Adisa-Shobi <s.oadisa.dev@gmail.com>
2022-07-04 11:43:39 +0000

    Task 10 Retry

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -3,7 +3,7 @@
 def divisible_by_2(my_list=[]):
     multipleOfTwo = []
     for i in my_list:
-        is_div = False;
+        is_div = False
         if (i % 2) == 0:
             is_div = True
         multipleOfTwo.append(is_div)
