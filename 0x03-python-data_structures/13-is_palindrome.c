9030964d86adecd05fcb243270e39a418c49464f
Adisa-Shobi <s.oadisa.dev@gmail.com>
2022-07-05 16:47:03 +0000

    Task 13 Retry

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -9,36 +9,26 @@
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *ptr = *head, *ptr_2;
-	int len = 0, i = 0, n, *entries;
+	return (is_palindromeUtil(*head, head));
+}
 
-	while (ptr)
-	{
-		len++;
-		ptr = ptr->next;
-	}
-	n = len / 2;
-	entries = malloc(sizeof(*entries) * (n + 1));
-	ptr_2 = *head;
-	while (ptr_2)
-	{
-		if (i < n)
-		{
-			entries[i] = ptr_2->n;
-		}
-		if (len % 2 != 0 && i == n)
-		{
-			ptr_2 = ptr_2->next;
-			i++;
-		}
-		if (i >= n)
-		{
-			if (entries[(len - 1) - i] != ptr_2->n)
-				return (0);
-		}
-		i++;
-		ptr_2 = ptr_2->next;
-	}
-	free(entries);
-	return (1);
+/**
+ *is_palindromeUtil - Used to find if a singly linked list is a palindrome
+ *@left: The left side of the list
+ *@right: The right side of the list
+ *Return: 1 if list is a palindrome, 0 if not
+ */
+int is_palindromeUtil(listint_t *right, listint_t **left)
+{
+	int is_p, is_p1;
+
+	if (right == NULL)
+		return (1);
+
+	is_p = is_palindromeUtil(right->next, left);
+	if (is_p == 0)
+		return (0);
+	is_p1 = (right->n == (*left)->n);
+	(*left) = (*left)->next;
+	return (is_p1);
 }
--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -18,6 +18,7 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+int is_palindromeUtil(listint_t *right, listint_t **left);
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
