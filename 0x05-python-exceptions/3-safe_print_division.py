e6d2e68258b000be87e46723ea8ced707b6cddd1
Adisa-Shobi <s.oadisa.dev@gmail.com>
2022-07-21 16:57:19 +0000

    Task 2: Retry

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
+    result = None
     try:
         result = a / b
     except (TypeError, ZeroDivisionError):
-        result = None
+        pass
     finally:
-        print("Inside Result: {}".format(result))
+        print("Inside result: {}".format(result))
     return(result)
