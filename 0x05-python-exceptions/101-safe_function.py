e35fa4896819779ac08281f6ad4afd3f832c5605
Adisa-Shobi <s.oadisa.dev@gmail.com>
2022-07-22 11:32:23 +0000

    Task 8 Retry

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -4,7 +4,7 @@ import sys
 def safe_function(fct, *args):
     try:
         result = fct(*args)
+        return (result)
     except:
-        result = None
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
-    return(result)
+        return (None)
