2bfccfef84357033c55fcc9c5df4961cdf011988
Adisa-Shobi <s.oadisa.dev@gmail.com>
2022-07-29 22:22:58 +0000

    Task 6 Retry

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -14,11 +14,18 @@ def text_indentation(text):
     '''
     if not(isinstance(text, str)):
         raise TypeError("text must be a string")
+
     i = 0
+    while(i < len(text) and text[i] == ' '):
+        i += 1
+
     while i < len(text):
         print(text[i], end="")
-        if text[i] in ('.', '?', ':'):
-            print('\n\n', end="")
-            if (((i + 1) < len(text)) and (text[i + 1] == " ")):
+        if text[i] == "\n" or text[i] in ".?:":
+            if text[i] in ('.', '?', ':'):
+                print('\n')
+            i += 1
+            while(i < len(text) and text[i] == ' '):
                 i += 1
+            continue
         i += 1
