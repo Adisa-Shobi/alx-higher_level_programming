ee895f37c59faf716d5c234b51dd34964552a10f
Adisa-Shobi <s.oadisa.dev@gmail.com>
2022-07-29 15:45:32 +0000

    Task 0 Retry

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,20 +1,15 @@
 #!/usr/bin/python3
 '''
 Function that add two integers
+
+
 '''
 
 
 def add_integer(a, b=98):
-    '''
-    Adds two integers, accepts float but converts to int
-
-    Args:
-       a (int): First integer to add
-       b (int): Second integer to add defaults to 98
-
-    Returns:
-       The sum of a and b
-    '''
+    '''Adds two integers, accepts float but converts to int
+    Args: a (int): First integer to add
+       b (int): Second integer to add defaults to 98'''
     if not (isinstance(a, int) or isinstance(a, float)):
         raise TypeError("a must be an integer")
     if not (isinstance(b, int) or isinstance(b, float)):
