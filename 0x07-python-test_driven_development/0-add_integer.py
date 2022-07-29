9745a08faa77d3ab7eb13ba142b59e59dc61230e
Adisa-Shobi <s.oadisa.dev@gmail.com>
2022-07-29 15:55:28 +0000

    Task 0 Retry

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -10,8 +10,8 @@ def add_integer(a, b=98):
     '''Adds two integers, accepts float but converts to int
     Args: a (int): First integer to add
        b (int): Second integer to add defaults to 98'''
-    if not (isinstance(a, int) or isinstance(a, float)):
+    if not isinstance(a, (float, int)):
         raise TypeError("a must be an integer")
-    if not (isinstance(b, int) or isinstance(b, float)):
+    if not isinstance(b, (float, int)):
         raise TypeError("b must be an integer")
     return int(a) + int(b)
