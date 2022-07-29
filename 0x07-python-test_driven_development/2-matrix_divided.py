b2c5b83b7762cfd56cdabc917d0f7f8458c9f2bb
Adisa-Shobi <s.oadisa.dev@gmail.com>
2022-07-29 22:24:59 +0000

    Task 2 Retry

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -20,8 +20,7 @@ def matrix_divided(matrix, div):
             and all(map(lambda x: isinstance(x, list), matrix))
             and all(map(lambda x: all(map(
                 lambda y: isinstance(y, (int, float)), x)), matrix))
-            and matrix != []
-            and all(map(lambda x: len(x) != 0, matrix))):
+            and matrix != []):
         raise TypeError("matrix must be a matrix"
                         " (list of lists) of integers/floats")
     if not (all(map(lambda x: (len(matrix[0]) == len(x)),
