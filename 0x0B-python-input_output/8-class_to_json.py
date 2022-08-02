a0ce26005b6d8c4fa41723f8e394e04f9c816d2d
Adisa-Shobi <s.oadisa.dev@gmail.com>
2022-08-02 20:46:51 +0000

    Python I/O

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''Defines function class_to_json
+'''
+import json
+
+
+def class_to_json(obj):
+    '''Writes the dictionary description for json serialization of an object
+    '''
+    return (obj.__dict__)
