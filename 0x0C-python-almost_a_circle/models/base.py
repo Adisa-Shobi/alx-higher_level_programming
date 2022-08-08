0b5ea860abf84abde351835cf448e897f23a313c
Adisa-Shobi <s.oadisa.dev@gmail.com>
2022-08-08 21:57:18 +0000

    Task 15 Retry

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -28,9 +28,10 @@ class Base:
         Args:
            list_dictionaries (list): List of dictionaries converted to JSON
         '''
-        if list_dictionaries is None or list_dictionaries == []:
+        if (list_dictionaries is None) or (list_dictionaries == []):
             return "[]"
-        return json.dumps(list_dictionaries)
+        else:
+            return json.dumps(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs):
