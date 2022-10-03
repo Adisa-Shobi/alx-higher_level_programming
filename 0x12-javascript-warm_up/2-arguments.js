cceaaa8694c968dfcedfeb975f478348740f61ca
Adisa-Shobi <s.oadisa.dev@gmail.com>
2022-10-03 17:25:55 +0000

    Update: '2-arguments.js' attempt to fix bug

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 
-const { argv } = require('node:process');
-
-if (argv.length <= 2) {
+const argLength = process.argv.length;
+if (argLength <= 2) {
   console.log('No argument');
-} else if (argv.length === 3) {
+} else if (argLength === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
