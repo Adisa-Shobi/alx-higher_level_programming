4316486203ea8e82d601deb506a263ced7953315
Adisa-Shobi <s.oadisa.dev@gmail.com>
2022-10-03 18:12:58 +0000

    Update: '11-second_biggest.js' attempt to fix bugs

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,14 +4,14 @@ const args = process.argv;
 if (args.length <= 3) {
   console.log(0);
 } else {
-    const nums = args.slice(2).map((num) => parseInt(num)).sort();
-    let second_largest = nums[0];
-    let largest = nums[0];
-    nums.forEach((value, index) => {
-	if (value > largest) {
-	    second_largest = largest;
-	    largest = value;
-	}
-    });
-    console.log(second_largest);
+  const nums = args.slice(2).map((num) => parseInt(num)).sort();
+  let secondLargest = nums[0];
+  let largest = nums[0];
+  nums.forEach((value, index) => {
+    if (value > largest) {
+      secondLargest = largest;
+      largest = value;
+    }
+  });
+  console.log(secondLargest);
 }
