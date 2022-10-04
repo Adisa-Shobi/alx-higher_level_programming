96c8b5543b5cb45a9329b23a91ed590ad5367993
Adisa-Shobi <s.oadisa.dev@gmail.com>
2022-10-04 19:56:14 +0000

    Create: '8-esrever.js' reverses lists

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+exports.esrever = function (list) {
+  for (let i = 0; i < (list.length / 2 >> 0); i++) {
+    [list[i], list[list.length - i - 1]] = [list[list.length - i - 1], list[i]];
+  }
+  return list;
+};
