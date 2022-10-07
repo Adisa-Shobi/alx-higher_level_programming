69296361baba257111afe9e2dd556f25d3bfd44f
Adisa-Shobi <s.oadisa.dev@gmail.com>
2022-10-07 22:13:00 +0000

    Create: 'model_city.py, 14-model_city_fetch_by_state.py' lists all cities with respective states

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''Defines ORM mapping for the city table
+'''
+
+from model_state import Base
+from sqlalchemy import Column, String, Integer, ForeignKey
+
+
+class City(Base):
+    '''Definition of city table mapping
+    '''
+    __tablename__ = "cities"
+
+    id = Column(Integer, unique=True, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
