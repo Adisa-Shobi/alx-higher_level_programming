071660179c73ea89f44f171b1b3142169243c649
Adisa-Shobi <s.oadisa.dev@gmail.com>
2022-10-21 19:06:19 +0000

    Update: '7-error_code.py' modified

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -9,6 +9,6 @@ if __name__ == "__main__":
     url = sys.argv[1]
     response = requests.get(url)
     if response.status_code >= 400:
-        print(f"Error code: {response.status_code}")
+        print("Error code: {}".format(response.status_code))
     else:
         print(response.text)
