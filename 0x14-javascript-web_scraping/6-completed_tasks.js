86270615727ed132ab94d214cda4493cba81ddb6
Adisa-Shobi <s.oadisa.dev@gmail.com>
2022-11-21 22:03:17 +0000

    Update: '6-completed_tasks.js' error handling upgraded

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,23 +1,26 @@
 #!/usr/bin/node
 
 const request = require('request');
-const url = 'https://jsonplaceholder.typicode.com/todos';
+const url = process.argv[2];
 const results = {};
 
 request.get(url, function (err, response, body) {
   if (err) {
     console.log(err);
-  }
-  const todoList = JSON.parse(body);
-  for (const item in todoList) {
-    if (todoList[item].completed) {
-      const task = todoList[item];
-      if (task.userId in results) {
-        results[task.userId]++;
-      } else {
-        results[task.userId] = 1;
+  } else if (response.statusCode == 200) {
+    const todoList = JSON.parse(body);
+    for (const item in todoList) {
+      if (todoList[item].completed) {
+	const task = todoList[item];
+	if (task.userId in results) {
+          results[task.userId]++;
+	} else {
+          results[task.userId] = 1;
+	}
       }
     }
+    console.log(results);
+  } else {
+    console.log("code:", response.statusCode);
   }
-  console.log(results);
 });
