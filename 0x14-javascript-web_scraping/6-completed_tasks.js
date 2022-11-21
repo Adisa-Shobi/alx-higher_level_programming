d197f2af3db8ad030e7b203e146d7b190832e2e2
Adisa-Shobi <s.oadisa.dev@gmail.com>
2022-11-21 22:05:46 +0000

    Update: '6-completed_tasks.js' code style modified to fit lint

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -7,20 +7,20 @@ const results = {};
 request.get(url, function (err, response, body) {
   if (err) {
     console.log(err);
-  } else if (response.statusCode == 200) {
+  } else if (response.statusCode === 200) {
     const todoList = JSON.parse(body);
     for (const item in todoList) {
       if (todoList[item].completed) {
-	const task = todoList[item];
-	if (task.userId in results) {
+        const task = todoList[item];
+        if (task.userId in results) {
           results[task.userId]++;
-	} else {
+        } else {
           results[task.userId] = 1;
-	}
+        }
       }
     }
     console.log(results);
   } else {
-    console.log("code:", response.statusCode);
+    console.log('code:', response.statusCode);
   }
 });
